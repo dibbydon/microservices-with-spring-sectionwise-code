060b5d6ee4edad143379fb0861bedff349410297
dibbydon <dibbydon@yahoo.com>
2023-01-04 14:02:50 +0000

    updated to dibbydon

---
+++ section5/cards/src/main/java/com/dibbydon/cards/controller/CardsController.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.dibbydon.cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dibbydon.cards.model.Cards;
+import com.dibbydon.cards.model.Customer;
+import com.dibbydon.cards.repository.CardsRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class CardsController {
+
+	@Autowired
+	private CardsRepository cardsRepository;
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+		if (cards != null) {
+			return cards;
+		} else {
+			return null;
+		}
+
+	}
+
+}
